eae05750cd6b1b9f0de58fd20d2947436a9864db
o-henry <cksgod16@gmail.com>
2020-12-28 21:05:23 +0900

    Add PrimaryButton

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,13 @@
 import styled from "styled-components";
 
+// Default (shared) Button
+// We'll inherit other button by this
 const PrimaryButton = styled.button`
-  // css
+  background-color: red;
+  border: none;
+  color: white;
+  padding: 12px 24px;
+  font-size: 1rem;
 `;
+
+export default PrimaryButton;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const App = () => <h1>Hello World</h1>;
+import PrimaryButton from "./components/Button";
+
+const App = () => <PrimaryButton>Hello World</PrimaryButton>;
 
 ReactDOM.render(<App />, document.querySelector("#root"));
+
+// cmd + d select all tag
