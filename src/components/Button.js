aaabe9959486793ebe4b26d27ee376f0cf342eb2
o-henry <cksgod16@gmail.com>
2020-12-28 21:06:22 +0900

    Add Inherit

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,16 @@ import styled from "styled-components";
 
 // Default (shared) Button
 // We'll inherit other button by this
-const PrimaryButton = styled.button`
+const Button = styled.button`
+  padding: 12px 24px;
+  font-size: 1rem;
+  border-radius: 2px;
+  min-width: 100px;
+  cursor: pointer;
+  font-family: "Roboto Mono", monospace;
+`;
+
+const PrimaryButton = styled(Button)`
   background-color: red;
   border: none;
   color: white;
